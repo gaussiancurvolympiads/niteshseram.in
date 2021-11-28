0c2cba38257b7e68d0e66bb61ba428a16b74e390
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-11-28 23:15:41 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -4,14 +4,13 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 
 const Blog = ({ posts }) => (
 	<Container
-		title='Blog | Nitesh Seram'
-		description='A digital corner sharing my thoughts, learning, and random things'
+		title='MS'
+		description=''
 	>
 		<div className='self-center max-w-full sm:max-w-[30rem] md:max-w-[42rem] my-8'>
 			<h1 className='heading'>Blog</h1>
 			<p>
-				My digital corner where I would be sharing my thoughts, learning, and
-				some random things.😅
+			
 			</p>
 			<ul className='mt-5'>
 				{posts.map((post, index) => (
