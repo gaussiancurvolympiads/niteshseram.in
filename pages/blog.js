813f1e79dcfbce79577332af12f1987a867677d0
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-11-28 23:09:47 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,20 +1,28 @@
+import BlogCard from '@/components/BlogCard'
 import Container from '@/layouts/Container'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
 
-
-const Blog => (
+const Blog = ({ posts }) => (
 	<Container
-		title='Blog'
-		description=''
+		title='Blog | Nitesh Seram'
+		description='A digital corner sharing my thoughts, learning, and random things'
 	>
 		<div className='self-center max-w-full sm:max-w-[30rem] md:max-w-[42rem] my-8'>
 			<h1 className='heading'>Blog</h1>
 			<p>
-			
+				
 			</p>
-			
+	
 		</div>
 	</Container>
 )
 
+export const getStaticProps = async () => {
+	let posts = await getAllFilesFrontMatter('blog')
+	posts.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
+	return {
+		props: { posts },
+	}
+}
 
 export default Blog
