efe1cd58868403d788da0fadad8d3a2930b407cb
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-11-30 23:50:56 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,7 @@ const FooterEvent = async (action, value) => {
 const Footer = () => (
 	<footer className='flex flex-col border-t-2 border-gray-200 dark:border-gray-800 py-12 px-8 sm:px-16 space-y-8 items-start md:items-center'>
 		<FooterContent />
-		<p>&copy; Nitesh Seram 2021</p>
+		<p>&copy; Mathematical Society 2021</p>
 	</footer>
 )
 
@@ -24,22 +24,22 @@ const FooterContent = () => (
 				<h4 className='font-bold'>PAGES</h4>
 				<FooterLink href='/'>Home</FooterLink>
 				<FooterLink href='/about'>About</FooterLink>
-				<FooterLink href='/projects'>Projects</FooterLink>
+				<FooterLink href='/projects'>Contest</FooterLink>
 				<FooterLink href='/blog'>Blog</FooterLink>
 			</div>
 			<div className='flex flex-col items-start ml-[100%] sm:ml-20 md:ml-40'>
 				<h4 className='font-bold'>SOCIALS</h4>
-				<FooterLink href='https://github.com/niteshseram'>Github</FooterLink>
-				<FooterLink href='https://twitter.com/niteshseram'>Twitter</FooterLink>
-				<FooterLink href='https://linkedin.com/in/niteshseram'>
+				<FooterLink href='https://mathematicssociety.vercel.app/'>Github</FooterLink>
+				<FooterLink href='https://mathematicssociety.vercel.app/'>Twitter</FooterLink>
+				<FooterLink href='https://mathematicssociety.vercel.app/'>
 					LinkedIn
 				</FooterLink>
 			</div>
 		</div>
 		<div className='flex flex-col items-start ml-0 sm:ml-20 md:ml-40 mt-12 sm:mt-0'>
-			<h4 className='font-bold'>EXTRA</h4>
-			<FooterLink href='https://github.com/niteshseram/niteshseram.in'>
-				Source Code
+			<h4 className='font-bold'>More</h4>
+			<FooterLink href='https://mathematicssociety.vercel.app/'>
+				Anything more you have?
 			</FooterLink>
 		</div>
 	</div>
