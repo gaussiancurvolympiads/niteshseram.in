9d4845d90e1341a339c500022d335ca22281a0b4
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-11-30 23:27:33 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -13,41 +13,41 @@ import {
 const projects = [
 	{
 		id: 1,
-		title: 'Portfolio V2',
+		title: 'Contests',
 		description:
-			'This site where you are right now is the 2nd iteration of my personal portfolio.',
+			'MS',
 		tools: [next, tailwind, MDX],
-		repo: 'https://niteshseram.in',
+		repo: 'https://mathematicssociety.vercel.app/',
 		feature: false,
 	},
 	{
 		id: 2,
-		title: 'Booksify',
+		title: 'Contest I name',
 		description:
-			'Booksify is a online book store where you can buy your favorite books. It is built using MERN stack and Redux with payment gateway integrated. It has all the major features like Admin Dashboard, Shopping Cart, Order etc.',
+			'Messi is not a good player',
 		tools: [react, node, monogdb],
-		live: 'https://booksify.herokuapp.com/',
-		repo: 'https://github.com/niteshseram/Booksify',
+		live: 'https://mathematicssociety.vercel.app/',
+		repo: 'https://mathematicssociety.vercel.app/',
 		feature: true,
 	},
 	{
 		id: 3,
-		title: 'IPL Dashboard',
+		title: 'LifeISPhysics',
 		description:
-			'IPL Dashboard project allows you to browse your favorite IPL teams and access their past games details, wins and losses ratios accessible by team and tournament year.',
+			'Life is not physics okay!',
 		tools: [react, springboot, hsql],
-		live: 'https://ipl-dashboard-97.netlify.app/',
-		repo: 'https://github.com/niteshseram/IPLDashboard',
+		live: 'https://mathematicssociety.vercel.app/',
+		repo: 'https://mathematicssociety.vercel.app/',
 		feature: true,
 	},
 	{
 		id: 4,
-		title: 'Portfolio V1',
+		title: 'MS',
 		description:
-			'The first iteration of my portfolio built using NextJs and Styled Components',
+			'MS',
 		tools: [next, styledcomponents],
-		live: 'https://niteshseram-q42jyvlvl-niteshseram.vercel.app/',
-		repo: 'https://github.com/niteshseram/Portfolio',
+		live: 'https://mathematicssociety.vercel.app/',
+		repo: 'https://mathematicssociety.vercel.app/',
 		feature: false,
 	},
 ]
