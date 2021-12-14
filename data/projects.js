bf9f3e3d8aba9a8ca00380992c178b8013bf7c26
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-12-14 18:00:08 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,14 +1,4 @@
-import {
-	hsql,
-	MDX,
-	monogdb,
-	next,
-	node,
-	react,
-	springboot,
-	styledcomponents,
-	tailwind,
-} from '@/data/tools'
+
 
 const projects = [
 
@@ -17,9 +7,9 @@ const projects = [
 		title: 'RMMC',
 		description:
 			'Upcoming Contest',
-		tools: [next, next, next],
-		live: 'https://mathematicssociety.vercel.app/',
-		repo: 'https://mathematicssociety.vercel.app/',
+	
+		live: 'https://www.dropbox.com/s/8r8ugzahym38mn7/5_6136505038933066722.pdf?dl=0',
+		repo: 'https://www.dropbox.com/s/8r8ugzahym38mn7/5_6136505038933066722.pdf?dl=0',
 		feature: true,
 	},
 	{
@@ -27,9 +17,9 @@ const projects = [
 		title: 'MSPTMO',
 		description:
 			'Previous Contest',
-		tools: [next, next, next],
-		live: 'https://mathematicssociety.vercel.app/',
-		repo: 'https://mathematicssociety.vercel.app/',
+		
+		live: 'https://www.dropbox.com/s/31xa1t6l5ns28ql/5_6201588503730455281%20%281%29.pdf?dl=0',
+		repo: 'https://www.dropbox.com/s/31xa1t6l5ns28ql/5_6201588503730455281%20%281%29.pdf?dl=0',
 		feature: true,
 	},
 
