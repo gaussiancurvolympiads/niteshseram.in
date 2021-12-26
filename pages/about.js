1d5135cc9a1c45013d2db4621d25f0510f8cd4d1
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-12-26 18:16:37 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -24,11 +24,11 @@ const Intro = () => (
 		<h2 className='heading text-xl md:text-2xl mt-8 mb-4'>What we do?</h2>
   <br></br>
 		<p className='mb-2 md:mb-4'>
-  <p>Do you love non routine mathematics, do you have a spark of problem solving , do you love to be engaged in problem solving
+ Do you love non routine mathematics, do you have a spark of problem solving , do you love to be engaged in problem solving
 <br></br>
  Do you want to be a part of the best mathematical society?
 <br></br>
-Then JOIN the mathematical society! We're an inviting Discord server and  ready to help YOU succeed in the world of logics , mathematics and improve your problem solving skills and thinking capability.
+Then JOIN the mathematical society! We are an inviting Discord server and  ready to help YOU succeed in the world of logics , mathematics and improve your problem solving skills and thinking capability.
 <br></br>
 We have tons of resources to help you :
 - challenges (potd, potw,qotd which are daily basis problems that make you ready for the best problems to face)
@@ -36,13 +36,12 @@ We have tons of resources to help you :
 - a welcoming and friendly community
 - resources to help you to actually be productive
 - live solve and marathons that are held by our admins , that deliver you quality content LIVE!
-- contests like MSO and MSO(2.0)  that challenges you make you exam ready!
+- contests like MSO and MSO  that challenges you make you exam ready!
 - our own made handouts that are probably best material for olympiads ( that is also  proven as our problem came nmtc stage1 and 2 both!!)                                                                                                                                                                                                   - a welcoming group of students that will solve your every academic doubts
  - and many more that you will get by exploring our server , basically we are a group of students that wants  to spread the light of mathematics in the darks to enlight the kingdom of knowledge!                                                                                                                 We hope you join us and enjoy the art of problem solving!
 <br></br>
 Links:
-Discord Server: <p href="https://discord.gg/PFETTeZ9rk"> Server Link </p>
-</p>		
+Discord Server: <p href="https://discord.gg/PFETTeZ9rk"> Server Link </p>	
 		</p>
 		<p>
 			
