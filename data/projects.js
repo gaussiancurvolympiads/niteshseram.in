0fb190a42f2e3500ce60bd7f24a7eae243b86b2b
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-12-26 18:32:13 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -18,7 +18,7 @@ const projects = [
 		title: 'RMMC',
 		description:
 			'Upcoming Contest',
-	
+		tools: [next,next, next],
 		live: 'https://www.dropbox.com/s/8r8ugzahym38mn7/5_6136505038933066722.pdf?dl=0',
 		repo: 'https://www.dropbox.com/s/8r8ugzahym38mn7/5_6136505038933066722.pdf?dl=0',
 		feature: true,
