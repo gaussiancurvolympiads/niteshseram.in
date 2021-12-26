717e518f1ad597455fabf87c8195459fcf931f82
gaussiancurvolympiads <79222966+gaussiancurvolympiads@users.noreply.github.com>
2021-12-26 18:20:14 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -25,23 +25,23 @@ const Intro = () => (
   <br></br>
 		<p className='mb-2 md:mb-4'>
  Do you love non routine mathematics, do you have a spark of problem solving , do you love to be engaged in problem solving
-<br></br>
+<br></br><br></br>
  Do you want to be a part of the best mathematical society?
-<br></br>
+<br></br><br></br>
 Then JOIN the mathematical society! We are an inviting Discord server and  ready to help YOU succeed in the world of logics , mathematics and improve your problem solving skills and thinking capability.
-<br></br>
-We have tons of resources to help you :
-- challenges (potd, potw,qotd which are daily basis problems that make you ready for the best problems to face)
-- bot with commands ranging from isl marabot to question bot
-- a welcoming and friendly community
-- resources to help you to actually be productive
-- live solve and marathons that are held by our admins , that deliver you quality content LIVE!
-- contests like MSO and MSO  that challenges you make you exam ready!
-- our own made handouts that are probably best material for olympiads ( that is also  proven as our problem came nmtc stage 1 and 2 both!!)                                                                                                                                                                                                   - a welcoming group of students that will solve your every academic doubts
+<br></br><br></br>
+We have tons of resources to help you : <br></br><br></br>
+- challenges (potd, potw,qotd which are daily basis problems that make you ready for the best problems to face)<br></br><br></br>
+- bot with commands ranging from isl marabot to question bot<br></br><br></br>
+- a welcoming and friendly community <br></br><br></br>
+- resources to help you to actually be productive<br></br><br></br>
+- live solve and marathons that are held by our admins , that deliver you quality content LIVE!<br></br><br></br>
+- contests like MSO and MSO  that challenges you make you exam ready!<br></br><br></br>
+- our own made handouts that are probably best material for olympiads ( that is also  proven as our problem came nmtc stage 1 and 2 both!!)   <br></br>      <br></br>                                                                                                                                                                                          - a welcoming group of students that will solve your every academic doubts
  - and many more that you will get by exploring our server , basically we are a group of students that wants  to spread the light of mathematics in the darks to enlight the kingdom of knowledge!                                                                                                                 We hope you join us and enjoy the art of problem solving!
-<br></br>
-Links:
-Discord Server: <p href="https://discord.gg/PFETTeZ9rk"> Server Link </p>	
+<br></br><br></br>
+Links: <br></br>
+Discord Server: <a href="https://discord.gg/PFETTeZ9rk"> Server Link </a>	
 		</p>
 		<p>
 			
